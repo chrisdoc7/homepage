443e461e402db441032e73242882dbfe5a004d15
Christian Ospina <chrisdoc7@hotmail.com>
2024-06-01 13:04:25 -0500

    Turn off/on skill added to homepage

--- main.js
+++ main.js
@@ -2,4 +2,8 @@ jQuery(function($) {
   $('.skill_rotate').click(function() {
     $('body').toggleClass('rotate');
   })
+
+  $('.skill_turn_lights').click(function() {
+    $('.light').toggleClass('off')
+  })
 })
