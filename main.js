bc50be60e07fe1a84bbc7f1d17399d94065f43be
Christian Ospina <chrisdoc7@hotmail.com>
2024-06-01 13:26:35 -0500

    Multiply skill added to homepage

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 jQuery(function($) {
   $('.skill_rotate').click(function() {
     $('body').toggleClass('rotate');
-  })
+  });
 
   $('.skill_turn_lights').click(function() {
-    $('.light').toggleClass('off')
+    $('.light').toggleClass('off');
+  });
+
+  $('body').on('click', '.skill_multiply', function() {
+    $(this).clone().insertAfter(this);
   })
 })
