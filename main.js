577178a2960cdee5678d26147422c31f8ceecdda
Christian Ospina <chrisdoc7@hotmail.com>
2024-06-01 21:28:19 -0500

    Get cats skill added to homepage

--- main.js
+++ main.js
@@ -14,4 +14,16 @@ jQuery(function($) {
   $('.skill_divide').click(function() {
     $(this).find('span').show();
   });
+
+  $('.skill_get_cats').click(function() {
+    $('.cats svg').show();
+    $('.cats div').html('');
+    $.ajax('https://api.thecatapi.com/v1/images/search').done(function(data) {
+      let cat_image_url = data[0].url;
+      let cat_image_width = data[0].width;
+      let cat_image_height = data[0].height;
+      $('.cats div').html(`<img width="${cat_image_width}" height="${cat_image_height}" src="${cat_image_url}">`);
+      $('.cats svg').hide();
+    });
+  });
 })
